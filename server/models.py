33eb52821757fb0fa5038ad33abccc0998c12745
Brian Bissette <bbissette3@yahoo.com>
2023-04-26 12:48:44 -0500

    lab complete

--- server/models.py
+++ server/models.py
@@ -6,6 +6,24 @@ class Author(db.Model):
     __tablename__ = 'authors'
     # Add validations and constraints 
 
+    @validates('name')
+    def validate_name(self, key, name):
+        names = db.session.query(Author.name).all()
+        if not name:
+            raise ValueError('Name cannot be empty')
+        
+        elif name in names:
+            raise ValueError('Name already exists')
+        
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError("Phone number must be 10 digits.")
+        
+        return phone_number
+
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
     phone_number = db.Column(db.String)
@@ -18,6 +36,32 @@ class Author(db.Model):
 class Post(db.Model):
     __tablename__ = 'posts'
     # Add validations and constraints 
+
+    @validates('content', 'summary')
+    def validate_length(self, key, string):
+        if (key == 'content') and (len(string) <= 250):
+            raise ValueError("Post content must be at least 250 characters long")
+        
+        if (key =='summary') and (len(string) >= 250):
+            raise ValueError("Post summary is a maximum of 250 characters long")
+    
+        return string
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError("Category must be Fiction or Non-Fiction")
+
+        return category
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+
+        if not any(substring in title for substring in clickbait):
+            raise ValueError("No clickbait found")
+
+        return title
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
